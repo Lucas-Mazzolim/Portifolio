d5ae1b770b9f082cc5e17933c1f0c051a3c1d09c
= <=>
2025-05-23 16:53:06 -0300

    Hability slide correction

--- portifolio/src/Main Components/Abilities.jsx
+++ portifolio/src/Main Components/Abilities.jsx
@@ -1,20 +1,30 @@
 import AbilitiesCard from "./AbilitiesCard";
 import '../styles/Abilities.css'
 import Habilities from '../assets/Habilities.json'
-import python from '../../public/Python.png'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function Abilities(){
     return(
         <section className = "Abilities">
+            <h2>Tools I Know</h2>
             <div className = "Abilities-Container">
-                <h2>Tools I Know</h2>
-                <div className = "Abilities-Container-Abilities">
-                    <AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/>
-                    <AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/>
-                    <AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/>
-                    <AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/>
-                    <AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/>
-                </div>     
+                    <Swiper
+                        breakpoints={{
+                        320: {slidesPerView: 1.5},
+                        600: {slidesPerView: 3, spaceBetween: 100},
+                        1000: {slidesPerView: 4, spaceBetween: 100}
+                    }}
+                        grabCursor = {true}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)}
+                    >
+                        <SwiperSlide><AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/></SwiperSlide>
+                        <SwiperSlide><AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/></SwiperSlide>
+                        <SwiperSlide><AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/></SwiperSlide>
+                        <SwiperSlide><AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/></SwiperSlide>
+                        <SwiperSlide><AbilitiesCard habilityDescription = {Habilities.h1.Description} habilityName = {Habilities.h1.Name} img = {Habilities.h1.ImagePath}/></SwiperSlide>
+                    </Swiper>
             </div>
         </section>
     )
